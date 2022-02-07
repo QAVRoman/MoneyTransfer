629cc3e0429520c38fe0605328881edd59277e46
Vakulka Roman <rvakulka@gmail.com>
2022-02-07 19:43:12 +0300

    First

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAccountBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int total = currentAccountBalance + transferAmount;
+        System.out.println(total);
+    }
+}
